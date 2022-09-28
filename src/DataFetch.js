2b48405e6496b815c985c538d871d53e46c33354
CRodr86 <rodrigocfotografia@gmail.com>
2022-09-28 09:29:26 +0200

    Incluir DataFetch

---
+++ src/DataFetch.js
@@ -0,0 +1,48 @@
+import Proptypes from "prop-types";
+import { useEffect, useState } from "react";
+
+const DataFetch = (props) => {
+  const [apiData, setApiData] = useState("");
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(`${props.url}`, {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        });
+        if (response.ok) {
+          const data = await response.json();
+          setApiData(data);
+          console.log(data);
+          setError(null);
+        } else {
+          setError("Hubo un error al obtener la petición");
+        }
+      } catch (error) {
+        setError("No pudimos hacer la solicitud para obtener los datos");
+      }
+    }
+    fetchData();
+  }, [props.url]);
+
+  if (error) {
+    return (
+      <div className="DataFetch">
+        <h1>{error}</h1>
+      </div>
+    );
+  }
+  return (
+    <div className="DataFetch">
+        <p>{JSON.stringify(apiData)}</p>
+    </div>
+  )
+};
+
+DataFetch.propTypes = {
+    url: Proptypes.string
+};
+
+export default DataFetch;
